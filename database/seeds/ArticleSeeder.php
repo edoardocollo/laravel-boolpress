92b3c1de4a4e83369bcd2064d93fb733af5d7d85
edoardo <collo>
2021-02-25 18:12:08 +0100

    review

--- database/seeds/ArticleSeeder.php
+++ database/seeds/ArticleSeeder.php
@@ -16,7 +16,7 @@ class ArticleSeeder extends Seeder
           $newArticle = new Article;
           $newArticle->titolo = $faker->sentence(3);
           $newArticle->autore = $faker->name();
-          $newArticle->contenuto = $faker->paragraph(4);
+          $newArticle->contenuto = $faker->paragraph(20);
           $newArticle->category_id = $faker->randomDigitNotNull();
           $newArticle->img = 'https://placeimg.com/640/480/tech'.rand(1,100);
 
